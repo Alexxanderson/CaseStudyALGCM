b0b6dea80a7edcd750cb7abc85b609c2d3eb8779
Alexxanderson <bien_aaron_miranda@dlsu.edu.ph>
2023-08-01 17:07:57 +0800

    initialization

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+# Actual main function
+def main():
+    # read txt file and print 2d maze grid
+    print("Yo")
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
